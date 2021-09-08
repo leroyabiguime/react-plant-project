2a988fff803442f0679f57d1310a6888a3c6c91b
mataa-hewa-leroy <mataa.abiguime17@gmail.com>
2021-09-08 17:58:34 +0000

    cart update

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,30 @@
+import { useState } from 'react/cjs/react.development'
 import '../styles/Cart.css'
 
 function Cart() {
     const monsteraPrice = 8
-    const ivyPrice = 10
-    const flowerPrice = 15
-    return (
-        <div className="lmj-cart ">
+    const [cart, updateCart] = useState(0)
+    const [isOpen, setIsOpen] = useState(true)
+
+    return isOpen ? (
+        <div className="lmj-cart">
             <h2>Panier</h2>
-            <ul>
-                <li>Monstera: {monsteraPrice}</li>
-                <li>Lierre: {ivyPrice}</li>
-                <li>Fleurs: {flowerPrice}</li>
-            </ul>
-            Total: {monsteraPrice + ivyPrice + flowerPrice} $
+            <div>
+                Montsera: {monsteraPrice}$
+                <button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+            </div>
+           <h3>
+           Total: {monsteraPrice * cart} $
+           </h3>
+
+            <div>
+                <button onClick={()=> updateCart(0)}>Vider le panier</button>
+            </div>
+           
         </div>
+    ):
+    (
+        <button onClick={()=> setIsOpen(true)}>Ouvrir le panier</button>
     )
 }
 export default Cart
